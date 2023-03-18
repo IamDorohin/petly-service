e4edaa0694e9d1423d12ada3a5c4add82f5fa244
IamDorohin <iamdorohin@gmail.com>
2023-03-18 02:44:08 +0200

    Installed & imported "modern normalize"

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
+import '../node_modules/modern-normalize/modern-normalize.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
