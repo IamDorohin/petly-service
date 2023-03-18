efe29fa4d194b8a0788a1f172f77d4438561b746
IamDorohin <iamdorohin@gmail.com>
2023-03-18 02:37:27 +0200

    Installed additional libraries & base pages and components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { SharedLayout } from 'components/SharedLayout/SharedLayout';
+
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
+const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
+const OurFriendsPage = lazy(() =>
+  import('pages/OurFriendsPage/OurFriendsPage')
+);
+const NewsPage = lazy(() => import('pages/NewsPage/NewsPage'));
+const NoticesPage = lazy(() => import('pages/NoticesPage/NoticesPage'));
+const UserPage = lazy(() => import('pages/UserPage/UserPage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/friends" element={<OurFriendsPage />} />
+        <Route path="/news" element={<NewsPage />} />
+        <Route path="//notices/:categoryName" element={<NoticesPage />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 };
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,3 @@
+const NotFoundPage = () => {};
+
+export default NotFoundPage;
