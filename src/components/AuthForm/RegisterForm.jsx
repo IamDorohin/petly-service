a4fdf7b89f47831240dbaa8d36071063cc1d1d0a
Oksana Semko <osemko@gmail.com>
2023-03-19 12:50:46 +0200

    Added AuthForm _ not yet styled

---
+++ src/components/AuthForm/RegisterForm.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+// import { login, register } from 'redux/auth/auth-operations';
+// import { errorToast, successToast } from 'variables/Toast';
