5c132ba2cd30f5c444ab484bdd9a4915e2c2907d
Oksana Semko <osemko@gmail.com>
2023-03-19 16:31:55 +0200

    Added auth-selectors

---
+++ src/redux/auth/auth-selectors.js
@@ -0,0 +1,3 @@
+export const selectIsLoggedIn = ({ auth }) => auth.isLoggedIn;
+export const selectIsRefreshing = ({ auth }) => auth.isRefreshing;
+export const selecttUser = ({ auth }) => auth.user;
