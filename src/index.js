5b209bf85df49cc333e865774746c1314e059336
IamDorohin <iamdorohin@gmail.com>
2023-03-20 15:31:28 +0200

    initiated theming feature

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import '../node_modules/modern-normalize/modern-normalize.css';
 import './index.css';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from 'theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/petly-service">
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
