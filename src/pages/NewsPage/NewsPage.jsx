0cebd2e66ee085ea227ade845b8299325921c15f
Danylo Osadchenko <dm.osadchenko@gmail.com>
2023-03-23 19:36:50 +0100

    update API

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,21 +1,20 @@
 import React, {useState} from 'react'
-import news from '../../data/news'
 import Title from '../../components/Generic/Title'
 import NewsSearch from '../../components/Generic/Search'
 import NewsGrid from '../../components/News/NewsGrid'
 import NewsCard from '../../components/News/NewsCard'
 import moment from 'moment/moment'
 import {NewsContainer, Section, Error} from './NewsPage.styled'
-// import {useGetNewsQuery} from '../../redux/news/NewsAPI'
+import {useGetNewsQuery} from '../../redux/news/NewsAPI'
 
 const NewsPage = () => {
 
     const [filter, setFilter] = useState('');
-    // const { data, error, isLoading } = useGetNewsQuery();
+    const { data, error, isLoading } = useGetNewsQuery();
 
     let visibleNews = {};
-    if (news) {
-    visibleNews = news.filter(item =>
+    if (data) {
+    visibleNews = data.filter(item =>
         item.title.toLowerCase().includes(filter) || item.description.toLowerCase().includes(filter)
     ).sort((a, b) => moment(b.date) - moment(a.date));
     };
@@ -23,11 +22,13 @@ const NewsPage = () => {
     return <Section>
         <NewsContainer>
         <Title>News</Title>
-        <NewsSearch
-            saveFilter={setFilter}
-        />
-        <NewsGrid>
-            {news &&
+            <NewsSearch saveFilter={setFilter} />
+            
+
+            {/* {error && Error component} */}
+            {/* {isLoading &&  Loader component} */}
+            <NewsGrid>
+            {data && 
                     visibleNews.map(item => {
             console.log(moment(item.date))
             return (
@@ -40,8 +41,8 @@ const NewsPage = () => {
                     
                 />
             );
-        })}
-        </NewsGrid>
+                    })}
+            </NewsGrid>
         {visibleNews.length === 0 && <Error>Sorry, we have no news with this keyword. Please, try another word.</Error>}
     </NewsContainer>
     </Section>
--- src/redux/news/NewsAPI.js
+++ src/redux/news/NewsAPI.js
@@ -1,18 +1,28 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-// Define a service using a base URL and expected endpoints
 export const NewsAPI = createApi({
   reducerPath: 'NewsAPI',
-    baseQuery: fetchBaseQuery({ baseUrl: '' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://petly-service-backend.onrender.com' }),
   tagTypes: ['News'],
   endpoints: (builder) => ({
     getContacts: builder.query({
-      query: () => `/news`,
+      query: () => `/api/news`,
       providesTags: ['News'],
     }),
   }),
 })
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
 export const { useGetNewsQuery } = NewsAPI;
+
+
+
+// import { configureStore } from '@reduxjs/toolkit'
+
+
+// export const store = configureStore({
+//   reducer: {
+
+//     [NewsAPI.reducerPath]: NewsAPI.reducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware().concat(NewsAPI.middleware),})
