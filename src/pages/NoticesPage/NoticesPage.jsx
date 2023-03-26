d5b07f4ce3998107831a4e4706d1f2c42eacc966
IamDorohin <iamdorohin@gmail.com>
2023-03-26 15:18:24 +0300

    Optimized notice page logic

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,9 +1,9 @@
-import { useLocation } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-import { useState } from 'react';
+// import { useLocation } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-import { useGetNoticesBySearchQuery } from 'redux/notices/noticesSlice';
+// import { useGetNoticesBySearchQuery } from 'redux/notices/noticesSlice';
 
 import Title from 'components/Generic/Title';
 import { NoticesSearch } from 'components/Notices/NoticesSearch/NoticesSearch';
@@ -18,42 +18,31 @@ import {
 } from 'pages/NoticesPage/NoticesPage.styled';
 
 const NoticesPage = () => {
-  const location = useLocation();
-  const { categoryName } = useParams();
-
   const [searchQuery, setSearchQuery] = useState('');
 
-  const [searchParams, setSearchParams] = useSearchParams();
-  const value = searchParams.toString();
-  const searchQueryState = searchParams.get('search') ?? '';
+  const [_, setSearchParams] = useSearchParams();
 
-  const handleSearchQueryChange = query => {
-    setSearchQuery(query);
-    setSearchParams(query !== '' ? { search: query } : {});
-  };
-
-  const { data } = useGetNoticesBySearchQuery(
-    { currentCategory: categoryName, currentSearch: value },
-    {
-      skip: searchQuery === '',
-      refetchOnMountOrArgChange: true,
+  useEffect(() => {
+    if (searchQuery !== '') {
+      setSearchParams({ search: searchQuery });
+    } else {
+      setSearchParams();
     }
-  );
-  const newSearchNotices = data?.data?.notices;
+  }, [searchQuery, setSearchParams]);
 
   return (
     <NoticesPageContainer>
       <Container>
         <Title>Find your favorite pet</Title>
         <NoticesSearch
-          element={searchQueryState}
-          onSubmit={handleSearchQueryChange}
+          setSearchQuery={setSearchQuery}
+          searchQuery={searchQuery}
         />
         <NoticesPageNavBox>
-          <NoticesCategoriesNav location={location} />
+          <NoticesCategoriesNav />
           <NoticeAddButton />
         </NoticesPageNavBox>
-        <NoticesCategoriesList newSearchNotices={newSearchNotices} />
+        <NoticesCategoriesList />
       </Container>
     </NoticesPageContainer>
   );
