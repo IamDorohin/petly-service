b698fe5a4403284157e854769c43a2fe221f1696
IamDorohin <iamdorohin@gmail.com>
2023-03-27 20:33:21 +0300

    Changed "Our friends" fetch logic
    Disabled redux API and added fetch service

--- src/components/OurFriends/FriendsList.jsx
+++ src/components/OurFriends/FriendsList.jsx
@@ -1,34 +1,45 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import {
+  useDispatch,
+  // useSelector
+} from 'react-redux';
 import NoResult from 'components/Generic/NoResult/NoResult';
-import Loader from 'components/Loader';
+// import Loader from 'components/Loader';
 import Friend from './OurFriends';
 import { List, Item } from './OurFriends.styled';
-import {
-  selectFriends,
-  selectError,
-  selectLoadingStatus,
-} from 'redux/friends/selectors';
-import fetchFriends from 'redux/friends/operations';
+// import {
+//   selectFriends,
+//   selectError,
+//   selectLoadingStatus,
+// } from 'redux/friends/selectors';
+// import fetchFriends from 'redux/friends/operations';
+import { getOurFriends } from 'services/friendsAPI';
 
 const FriendsList = () => {
-  const isLoading = useSelector(selectLoadingStatus);
-  const friends = useSelector(selectFriends);
-  const error = useSelector(selectError);
+  // const isLoading = useSelector(selectLoadingStatus);
+  // const friends = useSelector(selectFriends);
+  // const error = useSelector(selectError);
   const dispatch = useDispatch();
 
+  const [friendsArray, setFriendsArray] = useState([]);
+
   useEffect(() => {
-    dispatch(fetchFriends());
+    // dispatch(fetchFriends());
+    getOurFriends()
+      .then(data => {
+        setFriendsArray(data.data.result);
+      })
+      .catch(error => console.log(error));
   }, [dispatch]);
 
   // console.log(friends)
 
   return (
     <List>
-      {error && <NoResult />}
-      {isLoading && <Loader />}
-      {friends.length > 0 &&
-        friends.map(friend => (
+      {friendsArray === [] && <NoResult />}
+      {/* {isLoading && <Loader />} */}
+      {friendsArray.length > 0 &&
+        friendsArray.map(friend => (
           <Item key={friend._id}>
             <Friend friend={friend} />
           </Item>
--- src/services/friendsAPI.js
+++ src/services/friendsAPI.js
@@ -1,3 +1,13 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://petly-service-backend.onrender.com/api';
+
+export const getOurFriends = async token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  const response = await axios.get(`/our_friends`);
+  return response.data;
+};
+
 // import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 // export const FriendsAPI = createApi({
