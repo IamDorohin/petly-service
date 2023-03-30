5398f9e1df00cc39ce3f6ede3b5412f85f2485f3
Danylo Osadchenko <dm.osadchenko@gmail.com>
2023-03-30 19:56:16 +0200

    add pigination

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,11 @@
 import { createTheme } from '@mui/material';
 
 export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#F59256',
+    },
+  },
   breakpoints: {
     values: {
       mobileResponsive: 320,
