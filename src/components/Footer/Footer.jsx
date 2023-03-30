ae4c301b64d4f795b40a693b8898cf7323e1ad88
leshazimoglyad <102586567+leshazimoglyad@users.noreply.github.com>
2023-03-30 20:49:28 +0300

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,202 @@
+import React, { useState } from 'react';
+import * as SC from './Footer.styled.js';
+
+import Modal from './FooterModal/FooterModal.jsx';
+import maksUrl from './FooterModal/Images/maks.jpg';
+import serhiiSlUrl from './FooterModal/Images/serhiiSl.jpg';
+import oleksandrUrl from './FooterModal/Images/oleksandr.jpg';
+import olegUrl from './FooterModal/Images/oleg.jpg';
+import olenaUrl from './FooterModal/Images/olena.jpg';
+import vitaliiUrl from './FooterModal/Images/vitalii.jpg';
+import andriiUrl from './FooterModal/Images/andrii.jpg';
+import nataliUrl from './FooterModal/Images/natali.jpg';
+import darinaUrl from './FooterModal/Images/darina.jpg';
+import ivanUrl from './FooterModal/Images/ivan.jpg';
+import mykolaUrl from './FooterModal/Images/mykola.jpg';
+import fedirUrl from './FooterModal/Images/fedor.jpg';
+import volodymyrUrl from './FooterModal/Images/volodymyr.jpg';
+import innaUrl from './FooterModal/Images/inna.jpg';
+import valeriiaUrl from './FooterModal/Images/valeriia.jpg';
+import veronikaUrl from './FooterModal/Images/veronika.jpg';
+
+export const Footer = () => {
+  const [modalActive, setModalActive] = useState(false);
+  return (
+    <SC.FooterContainer>
+      <SC.FooterSection>
+        <SC.FooterContent>© 2023 All Rights Reserved</SC.FooterContent>
+        <SC.FooterMenu>
+          <SC.MenuItem>
+            <SC.MenuItemLink
+              href="#"
+              className="open-btn"
+              onClick={() => setModalActive(true)}
+            >
+              * Team Students GoIT *
+            </SC.MenuItemLink>
+          </SC.MenuItem>
+        </SC.FooterMenu>
+        <Modal active={modalActive} setActive={setModalActive}>
+          <SC.TeamWrapper>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/gradussus"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage src={vitaliiUrl} loading="lazy" alt="Vitalii" />
+                <SC.TeamName>Vitalii</SC.TeamName>
+                <SC.TeamRole>Team Lead</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/LenaK513"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage src={olenaUrl} loading="lazy" alt="Olena" />
+                <SC.TeamName>Olena</SC.TeamName>
+                <SC.TeamRole>Scrum Master</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/IvanBlazhko"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage src={ivanUrl} loading="lazy" alt="Ivan" />
+                <SC.TeamName>Ivan</SC.TeamName>
+                <SC.TeamRole>Team Lead (front part)</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/Ghozzty"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage src={fedirUrl} loading="lazy" alt="Fedir" />
+                <SC.TeamName>Fedir</SC.TeamName>
+                <SC.TeamRole>Developer</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/Natali2721"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage src={nataliUrl} loading="lazy" alt="Natali" />
+                <SC.TeamName>Natali</SC.TeamName>
+                <SC.TeamRole>Developer</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/Slapchenko"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage src={serhiiSlUrl} loading="lazy" alt="Serhii" />
+                <SC.TeamName>Serhii</SC.TeamName>
+                <SC.TeamRole>Developer</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/AlexCheverda"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage
+                  src={oleksandrUrl}
+                  loading="lazy"
+                  alt="Oleksandr"
+                />
+                <SC.TeamName>Oleksandr</SC.TeamName>
+                <SC.TeamRole>Developer</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/andrii-pozniak"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage src={andriiUrl} loading="lazy" alt="Andrii" />
+                <SC.TeamName>Andrii</SC.TeamName>
+                <SC.TeamRole>Developer</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/Albertukraine"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage src={olegUrl} loading="lazy" alt="Oleg" />
+                <SC.TeamName>Oleg</SC.TeamName>
+                <SC.TeamRole>Developer</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/darinacat87"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage src={darinaUrl} loading="lazy" alt="Darina" />
+                <SC.TeamName>Darina</SC.TeamName>
+                <SC.TeamRole>Developer</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/maksnovakov"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage src={maksUrl} loading="lazy" alt="Maks" />
+                <SC.TeamName>Maks</SC.TeamName>
+                <SC.TeamRole>Developer</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamLink
+                href="https://github.com/MykolaPom"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <SC.TeamImage src={mykolaUrl} loading="lazy" alt="Mykola" />
+                <SC.TeamName>Mykola</SC.TeamName>
+                <SC.TeamRole>Developer</SC.TeamRole>
+              </SC.TeamLink>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamImage src={volodymyrUrl} loading="lazy" alt="Volodymyr" />
+              <SC.TeamName>Volodymyr</SC.TeamName>
+              <SC.TeamRole>Team Lead QA</SC.TeamRole>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamImage src={innaUrl} loading="lazy" alt="Inna" />
+              <SC.TeamName>Inna</SC.TeamName>
+              <SC.TeamRole>QA Engineer</SC.TeamRole>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamImage src={valeriiaUrl} loading="lazy" alt=" Valeriia" />
+              <SC.TeamName> Valeriia</SC.TeamName>
+              <SC.TeamRole>QA Engineer</SC.TeamRole>
+            </SC.TeamCard>
+            <SC.TeamCard>
+              <SC.TeamImage src={veronikaUrl} loading="lazy" alt="Veronika" />
+              <SC.TeamName>Veronika</SC.TeamName>
+              <SC.TeamRole>QA Engineer</SC.TeamRole>
+            </SC.TeamCard>
+          </SC.TeamWrapper>
+        </Modal>
+      </SC.FooterSection>
+    </SC.FooterContainer>
+  );
+};
---
+++ src/components/Footer/Footer.styled.js
@@ -0,0 +1,143 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  display: none;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    backdrop-filter: blur(20px);
+    background: hsla(0, 0%, 100%, 0.1);
+    bottom: 0;
+    left: 0;
+    box-shadow: -1px -5px 10px -1px rgba(0, 0, 0, 0.5);
+    display: flex;
+    font-size: 14px;
+    height: 30px;
+    position: fixed;
+    width: 100vw;
+    z-index: 1001;
+  }
+`;
+
+export const FooterSection = styled.div`
+  max-width: 1280px;
+  padding: 0 16px;
+  width: 100%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0;
+  flex-wrap: wrap;
+`;
+
+export const FooterContent = styled.p`
+  color: #fff;
+  text-align: center;
+  margin: 10px 20px;
+  font-size: 14px;
+`;
+
+export const FooterMenu = styled.ul`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 15px;
+  flex-wrap: wrap;
+`;
+
+export const MenuItem = styled.li`
+  list-style: none;
+`;
+
+export const MenuItemLink = styled.a`
+  font-size: 14px;
+  color: #fff;
+  margin: 0 10px;
+  display: inline-block;
+  transition: 0.5s;
+  text-decoration: none;
+  opacity: 0.75;
+  &:hover {
+    opacity: 1;
+    transform: translateY(-3px);
+  }
+`;
+
+/********** Modal */
+
+export const TeamWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  max-height: 95%;
+  width: 180px;
+  overflow: auto;
+  background-color: #fff;
+  border-radius: 15px;
+
+  @media screen and (min-width: 460px) {
+    width: 360px;
+  }
+  @media screen and (max-width: (768px - 1)) {
+    width: 420px;
+  }
+  @media screen and (min-width: 768px) {
+    margin: -30px;
+    width: 480px;
+    border-radius: 25px;
+  }
+  @media screen and (min-width: 1280px) {
+    width: 480px;
+  }
+`;
+
+export const TeamCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  color: inherit;
+  margin: 10px 15px;
+  width: calc((100% - 60px) / 2);
+
+  @media screen and (min-width: 768px) {
+    width: calc((100% - 120px) / 4);
+  }
+  @media screen and (min-width: 1280px) {
+    margin: 10px 20px;
+    width: calc((100% - 160px) / 4);
+  }
+  &:hover {
+    transform: translate(5px, 5px);
+  }
+`;
+
+export const TeamLink = styled.a`
+  color: #000;
+`;
+
+export const TeamImage = styled.img`
+  z-index: 100;
+  position: relative;
+  margin-bottom: 10px;
+  width: 100%;
+  border-radius: 50%;
+  box-shadow: 5px 5px 11px -2px #000000;
+`;
+
+export const TeamName = styled.p`
+  margin-bottom: 5px;
+  color: black;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 700;
+`;
+
+export const TeamRole = styled.p`
+  font-family: 'Roboto', sans-serif;
+  color: #545454;
+  @media screen and (max-width: (768px - 1)) {
+    font-size: 14px;
+  }
+`;
