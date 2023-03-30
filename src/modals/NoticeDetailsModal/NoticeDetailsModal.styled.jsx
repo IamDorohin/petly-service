875e96a63aa6c4dcfae00de4a4e9ce6a6d6ed7ce
vitalik poliakov <vitalik211287@gmail.com>
2023-03-30 21:22:32 +0300

    Update NoticeDetailsModal.styled.jsx

--- src/modals/NoticeDetailsModal/NoticeDetailsModal.styled.jsx
+++ src/modals/NoticeDetailsModal/NoticeDetailsModal.styled.jsx
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
 export const ModalBoxNotice = theme => ({
-//   overflow: 'scroll',
+  //   overflow: 'scroll',
   position: 'absolute',
   top: '50%',
   left: '50%',
+  padding: '60px 20px 40px 20px',
   transform: 'translate(-50%, -50%)',
   width: '280px',
   height: 'auto',
-  minHeight: '80vh',
+    minHeight: '80vh',
   bgcolor: '#FFFFFF',
   borderRadius: '20px',
   boxShadow: '7px 4px 14px rgba(0, 0, 0, 0.11)',
