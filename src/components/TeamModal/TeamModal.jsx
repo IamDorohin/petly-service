6700deaa0058534bdc8339ee03afca42c3a7d706
IamDorohin <iamdorohin@gmail.com>
2023-03-31 18:45:47 +0300

    commit

--- src/components/TeamModal/TeamModal.jsx
+++ src/components/TeamModal/TeamModal.jsx
@@ -1,43 +1,48 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
-import { ModalContainer, FirsModalContainer, Photo, Grid, Item, Name } from './TeamModal.styled';
-import danya from '../../images/team/developers/danya.jpg'
-import anastasia from '../../images/team/developers/anastasia.jpg'
-import andrii from '../../images/team/developers/andrii.jpg'
-import fil from '../../images/team/developers/fil.jpg'
-import kostya from '../../images/team/developers/kostya.jpg'
-import maks from '../../images/team/developers/maks.jpg'
-import mark from '../../images/team/developers/mark.jpg'
-import misha from '../../images/team/developers/misha.jpg'
-import oksana from '../../images/team/developers/oksana.jpg'
-import oleksiy from '../../images/team/developers/oleksiy.jpg'
-import sasha from '../../images/team/developers/sasha.jpg'
-import vitalii from '../../images/team/developers/vitalii.jpg'
-import yurii from '../../images/team/developers/yurii.jpg'
+import {
+  ModalContainer,
+  FirsModalContainer,
+  Photo,
+  Grid,
+  Item,
+  Name,
+} from './TeamModal.styled';
+import danya from '../../images/team/developers/danya.jpg';
+import anastasia from '../../images/team/developers/anastasia.jpg';
+import andrii from '../../images/team/developers/andrii.jpg';
+import fil from '../../images/team/developers/fil.jpg';
+import kostya from '../../images/team/developers/kostya.jpg';
+import maks from '../../images/team/developers/maks.jpg';
+import mark from '../../images/team/developers/mark.jpg';
+import misha from '../../images/team/developers/misha.jpg';
+import oksana from '../../images/team/developers/oksana.jpg';
+import oleksiy from '../../images/team/developers/oleksiy.jpg';
+import sasha from '../../images/team/developers/sasha.jpg';
+import vitalii from '../../images/team/developers/vitalii.jpg';
+import yurii from '../../images/team/developers/yurii.jpg';
 
-import natalia from '../../images/team/testers/natalia.jpg'
-import alina from '../../images/team/testers/alina.jpg'
-import maria from '../../images/team/testers/maria.jpg'
-import ovechko from '../../images/team/testers/ovechko.jpg'
-import pavlo from '../../images/team/testers/pavlo.jpg'
-import viktor from '../../images/team/testers/viktor.jpg'
+import natalia from '../../images/team/testers/natalia.jpg';
+import alina from '../../images/team/testers/alina.jpg';
+import maria from '../../images/team/testers/maria.jpg';
+import ovechko from '../../images/team/testers/ovechko.jpg';
+import pavlo from '../../images/team/testers/pavlo.jpg';
+import viktor from '../../images/team/testers/viktor.jpg';
 
-
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
-};
+// const style = {
+//   position: 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: 400,
+//   bgcolor: 'background.paper',
+//   border: '2px solid #000',
+//   boxShadow: 24,
+//   pt: 2,
+//   px: 4,
+//   pb: 3,
+// };
 
 function DevelopersModal() {
   const [open, setOpen] = useState(false);
@@ -61,56 +66,56 @@ function DevelopersModal() {
           <h2 id="child-modal-title">Our developers</h2>
           <Grid>
             <Item>
-              <Photo alt='team' src={kostya}/>
+              <Photo alt="team" src={kostya} />
               <Name>Костянтин Дорогін (Тім Лід)</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={andrii}/>
-              <Name>Андрій  Ужва</Name>
+              <Photo alt="team" src={andrii} />
+              <Name>Андрій Ужва</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={danya}/>
+              <Photo alt="team" src={danya} />
               <Name>Данило Осадченко</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={anastasia}/>
+              <Photo alt="team" src={anastasia} />
               <Name>Анастасія Недбай</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={oksana}/>
+              <Photo alt="team" src={oksana} />
               <Name>Оксана Семко</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={fil}/>
+              <Photo alt="team" src={fil} />
               <Name>Андрій Филь</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={maks}/>
+              <Photo alt="team" src={maks} />
               <Name>Максим Чуйков</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={mark}/>
+              <Photo alt="team" src={mark} />
               <Name>Марк Холод</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={misha}/>
+              <Photo alt="team" src={misha} />
               <Name>Михайло Гурак</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={oleksiy}/>
+              <Photo alt="team" src={oleksiy} />
               <Name>Олексій Зимогляд (Скрам)</Name>
             </Item>
-            
+
             <Item>
-              <Photo alt='team' src={vitalii}/>
+              <Photo alt="team" src={vitalii} />
               <Name>Віталій Поляков</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={yurii}/>
+              <Photo alt="team" src={yurii} />
               <Name>Юрій Аврамець</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={sasha}/>
+              <Photo alt="team" src={sasha} />
               <Name>Олександр Хмільневський</Name>
             </Item>
           </Grid>
@@ -143,27 +148,27 @@ function TestersModal() {
           <h2 id="child-modal-title">Our testers</h2>
           <Grid>
             <Item>
-              <Photo alt='team' src={natalia}/>
+              <Photo alt="team" src={natalia} />
               <Name>Наталія Костик (Тім Лід)</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={alina}/>
+              <Photo alt="team" src={alina} />
               <Name>Аліна Пристишко</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={maria}/>
+              <Photo alt="team" src={maria} />
               <Name>Марія Сидора</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={ovechko}/>
+              <Photo alt="team" src={ovechko} />
               <Name>Олександр Овечко</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={pavlo}/>
+              <Photo alt="team" src={pavlo} />
               <Name>Павло Кучеренко</Name>
             </Item>
             <Item>
-              <Photo alt='team' src={viktor}/>
+              <Photo alt="team" src={viktor} />
               <Name>Віктор Довгалюк</Name>
             </Item>
           </Grid>
